dc69e946b6bcea68b8657d521e95ac6c96095279
cosmicBboy <niels.bantilan@gmail.com>
2015-04-18 16:08:33 -0400

    added trainingCourses

---
+++ client/views/trainingCourses.js
@@ -0,0 +1,51 @@
+tempTrainingContent = [
+  {
+    'icon': 'fa-area-chart',
+    'title': 'TRADITIONAL STATISTICS',
+    'description': 'One-day theoretical course',
+    'panelId': 'traditional-stats',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'INTRO TO R',
+    'description': 'One-day programming course teaching how to perform' +
+      'material learned in the Traditional Statistics Course',
+    'panelId': 'intro-to-r',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'MODELING & ANALYTICS IN R',
+    'description': 'One-day course with advanced topics in R',
+    'panelId': 'modeling-analytics-r',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'DATA PRESENTATION AND PORTABILITY',
+    'description': 'One-day course about communicating code in R',
+    'panelId': 'intro-to-python',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'INTRO TO PYTHON',
+    'description': 'One-day course on the basics of computing and data in Python',
+    'panelId': 'intro-to-python',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'MACHINE LEARNING',
+    'description': 'One-day course combining theory and programming',
+    'panelId': 'machine-learning',
+  },
+  {
+    'icon': 'fa-area-chart',
+    'title': 'HIGH PERFORMANCE COMPUTING IN R',
+    'description': 'One-day course focused on speed improvements in R',
+    'panelId': 'high-perf-comp-r',
+  }
+]
+
+Template.trainingCourses.helpers({
+  trainingContent: function() {
+    return tempTrainingContent
+  }
+});
