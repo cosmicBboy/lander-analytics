ca7aba9b37d578822bf510949c1d4ce6c3f5400f
cosmicBboy <niels.bantilan@gmail.com>
2015-05-14 01:00:03 -0400

    About page is fully reactive

--- client/views/about.js
+++ client/views/about.js
@@ -1,25 +1,62 @@
+Meteor.subscribe('about');
+
 Template.onRendered(function() {
-  // var data = About.find({}).fetch();
-  // console.log(data);
+  console.log(this.data);
 });
 
 Template.about.helpers({
   showOverlay : function() {
     return Session.get('overlayTemplate');
+  },
+  title: function() {
+    var data = Template.currentData();
+    return data.title;
+  },
+  description: function() {
+    var data = Template.currentData();
+    return data.content;
+  },
+  leaders: function() {
+    var data = Template.currentData();
+    return data.leadership;
+  },
+  //leader data context
+  getPicture: function(arg) {
+    var id = arg.hash.pictureId;
+    console.log(Images.findOne({_id: id}));
+    return Images.findOne({_id: id}).url();
   }
 });
 
 Template.about.events({
-  'click #leader-jared' : function (e,t) {
-    Session.set('overlayTemplate', 'jaredBio');
-    $('body').addClass('noscroll');
+  'click a img' : function(e,t) {
+    Session.set('overlayTemplate', 'leaderBio');
+  }
+  // 'click #leader-jared' : function (e,t) {
+  //   Session.set('overlayTemplate', 'jaredBio');
+  //   $('body').addClass('noscroll');
+  // },
+  // 'click #leader-howard' : function (e,t) {
+  //   Session.set('overlayTemplate', 'howardBio');
+  //   $('body').addClass('noscroll');
+  // },
+  // 'click #leader-eric' : function (e,t) {
+  //   Session.set('overlayTemplate', 'ericBio');
+  //   $('body').addClass('noscroll');
+  // },
+})
+
+Template.leaderBio.helpers({
+  name: function() {
+    console.log("LEADER NAME", Template.parentData(0));
+    return Template.parentData(0).name;
   },
-  'click #leader-howard' : function (e,t) {
-    Session.set('overlayTemplate', 'howardBio');
-    $('body').addClass('noscroll');
+  title: function() {
+    console.log("LEADER TITLE", Template.parentData(0));
+    return Template.parentData(0).positionTitle;
   },
-  'click #leader-eric' : function (e,t) {
-    Session.set('overlayTemplate', 'ericBio');
-    $('body').addClass('noscroll');
-  },
+  bio: function() {
+    console.log("LEADER CONTENT", Template.parentData(0));
+    return Template.parentData(0).bio;
+  }
 })
