1e16b73f9b38d38cf90cca4169519d5b67e8ecde
cosmicBboy <niels.bantilan@gmail.com>
2015-05-22 00:39:08 -0400

    data talent page is now editable in the CMS

--- client/views/dataTalent.js
+++ client/views/dataTalent.js
@@ -1,28 +1,56 @@
 Template.dataTalent.helpers({
-  showOverlay : function() {
-    return Session.get('overlayTemplate');
+  showOverlay : function(arg) {
+    var clickedName = Session.get('talentName'),
+      checkName = arg.hash.talentName;
+
+    if (clickedName === checkName) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  dataTalentEven: function() {
+    var data = this.dataTalent,
+      slicer = data.length - (data.length % 3);
+    return data.slice(0, slicer);
+  },
+  dataTalentOdd: function() {
+    var data = this.dataTalent,
+      slicer = data.length - (data.length % 3);
+
+    return data.slice(slicer);
+  },
+  dataTalentOddSingle: function() {
+    var data = Template.parentData(1).dataTalent;
+    console.log('DATA ODD SINGLE', data);
+      slicer = data.length - (data.length % 3);
+    return data.slice(slicer).length === 1;
+  },
+  getPicture: function(arg) {
+    var id = arg.hash.pictureId;
+    console.log(Images.findOne({_id: id}));
+    return Images.findOne({_id: id}).url();
   }
 });
 
 Template.dataTalent.events({
-  'click #team-jared' : function (e,t) {
-    Session.set('overlayTemplate', 'jaredTalent');
-    $('body').addClass('noscroll');
+  'click a img' : function(e,t) {
+    var talent = $(e.target).parent('a').siblings('.caption').find('h4').text();
+    console.log('SETTING TALENT', talent);
+    // console.log(t.find('.caption h3'));
+    // console.log(t.$('.caption h3'));
+    Session.set('overlayTemplate', 'talentBio');
+    Session.set('talentName', talent);
+  }
+})
+
+Template.talentBio.helpers({
+  name: function() {
+    console.log("TALENT NAME", Template.parentData(0));
+    return Template.parentData(0).name;
   },
-  'click #team-andrew' : function (e,t) {
-    Session.set('overlayTemplate', 'andrewTalent');
-    $('body').addClass('noscroll');
-  },
-  'click #team-daniel' : function (e,t) {
-    Session.set('overlayTemplate', 'danielTalent');
-    $('body').addClass('noscroll');
-  },
-  'click #team-bob' : function (e,t) {
-    Session.set('overlayTemplate', 'bobTalent');
-    $('body').addClass('noscroll');
-  },
-  'click #team-vivian' : function (e,t) {
-    Session.set('overlayTemplate', 'vivianTalent');
-    $('body').addClass('noscroll');
-  },
+  bio: function() {
+    console.log("TALENT CONTENT", Template.parentData(0));
+    return Template.parentData(0).bio;
+  }
 })
--- server/publish.js
+++ server/publish.js
@@ -10,6 +10,10 @@ Meteor.publish('about', function() {
 	return About.find({});
 });
 
+Meteor.publish('data_talent', function() {
+	return DataTalent.find({});
+});
+
 Meteor.publish(null, function() {
   return Images.find();
 });
