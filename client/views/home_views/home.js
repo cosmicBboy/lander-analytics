9c9fc37df3ff4726044c1d0e7a87169a1c785d6f
cosmicBboy <niels.bantilan@gmail.com>
2015-06-06 20:09:40 -0400

    added setTimeout to owlCarousel

--- client/views/home_views/home.js
+++ client/views/home_views/home.js
@@ -28,23 +28,25 @@ var scrollTo = function(element, offset, targetClass) {
 
 Template.home.onRendered(function() {
   
-  $("#owl-demo").owlCarousel({
+  Meteor.setTimeout(function() {
+    $("#owl-demo").owlCarousel({
 
-    navigation : true, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    autoplay: 1000,
-    rewindNav: true,
-    rewindSpeed: 1000,
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      autoplay: 1000,
+      rewindNav: true,
+      rewindSpeed: 1000,
 
-    // "singleItem:true" is a shortcut for:
-    items : 1, 
-    itemsDesktop : false,
-    itemsDesktopSmall : false,
-    itemsTablet: false,
-    itemsMobile : false
+      // "singleItem:true" is a shortcut for:
+      items : 1, 
+      itemsDesktop : false,
+      itemsDesktopSmall : false,
+      itemsTablet: false,
+      itemsMobile : false
 
-  });
+    });
+  }, 200)
 
   Session.set('showTraining', true);
 
